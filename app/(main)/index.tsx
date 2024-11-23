85c26220d23bd86057645ab6e4098f0a3915f532
Thierno Diallo <dthiernox@gmail.com>
2024-11-22 23:16:42 -0500

    Implement geolocalisation

--- app/(main)/index.tsx
+++ app/(main)/index.tsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Redirect } from "expo-router";
-import { View, StyleSheet, Image, Text } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  AppStateStatus,
+  AppState,
+} from "react-native";
 
 import * as Location from "expo-location";
 import axios from "axios";
@@ -16,20 +23,9 @@ import ScrollableLayout from "@/components/layouts/ScrollableLayout";
 import FilterModalLayout from "@/components/layouts/FilterModalLayout";
 import SelectLocalisation from "@/components/common/SelectLocalisation";
 import CardScrollableLayout from "@/components/layouts/CardScrollableLayout";
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 export default function HomeScreen() {
-  /*
-    
-Pavillon de la Faculté de l'aménagement
-Pavillon de la Faculté de musique
-Pavillon J.-Armand-Bombardier
-Pavillon Marcelle-Coutu
-Pavillon Samuel-Bronfman
-Pavillon Maximilien-Caron
-Pavillon René-J.-A.-Lévesque
-Pavillon Liliane-de-Stewart
-Pavillon de la Direction des immeubles
-    */
   const [pavillons, setPavillons] = useState([
     "Pavillon Roger-Gaudry",
     "Pavillon Jean-Coutu",
@@ -42,27 +38,103 @@ Pavillon de la Direction des immeubles
   const [coordinates, setCoordinates] = useState<
     { pavillon: string; lat: number; lng: number }[]
   >([
-      { pavillon: "Pavillon Jean-Coutu", lat: 45.500485542287784, lng: -73.61474544264088 },
-      { pavillon: "Pavillon Jean-Brillant", lat: 45.49865436288935, lng: -73.61837937589661 },
-      { pavillon: "Pavillon Roger-Gaudry", lat: 45.502683040350085, lng: -73.61583834278646 },
-      { pavillon: "Pavillon André-Aisenstadt", lat: 45.501118321893316, lng:  -73.6158027337848 },
-      { pavillon: "Pavillon Claire-McNicoll", lat: 45.50177979562059, lng: -73.61654496899769 },
-      
-      { pavillon: "Pavillon Lionel-Groulx", lat: 45.4992286213827, lng: -73.61811440328101 },
-      { pavillon: "Pavillon Marie-Victorin", lat: 45.510619686954136, lng: -73.61164866212512 },
-      { pavillon: "Pavillon Marguerite-D'Youville", lat: 45.509398466364736, lng: -73.61839316372183},
-      { pavillon: "Pavillon Paul-G.-Desmarais", lat: 45.500396996801676, lng: -73.61633066502466 },
-      { pavillon: "Pavillon J.-A.-DeSève", lat: 45.50714348820753, lng: -73.61441639453919 },
-      { pavillon: "Pavillon de la Faculté de l'aménagement", lat: 45.50463484932084, lng: -73.62119293577463 },
-      { pavillon: "Pavillon de la Faculté de musique", lat: 45.50929993324537, lng: -73.60856773744851 },
-      { pavillon: "Pavillon J.-Armand-Bombardier", lat: 45.50319960914957, lng: -73.61299058051966 },
-      { pavillon: "Pavillon Marcelle-Coutu", lat: 45.49999697231997, lng: -73.61521747657558 },
-      { pavillon: "Pavillon Samuel-Bronfman", lat: 45.49942661426102, lng: -73.61633843067683 },
-      { pavillon: "Pavillon Maximilien-Caron", lat: 45.49864611249791, lng: -73.61698298364878 },
-      { pavillon: "Pavillon René-J.-A.-Lévesque", lat: 45.5013693622242, lng: -73.61503855240562 },
-      { pavillon: "Pavillon Liliane-de-Stewart", lat: 45.50972366912569, lng: -73.61924582738257 },
-      { pavillon: "Pavillon de la Direction des immeubles", lat: 45.50216858742022, lng: -73.61397216405996 },
-      { pavillon: "Campus Mil", lat: 45.5231172783649, lng: -73.61966164943738 },
+    {
+      pavillon: "Pavillon Jean-Coutu",
+      lat: 45.500485542287784,
+      lng: -73.61474544264088,
+    },
+    {
+      pavillon: "Pavillon Jean-Brillant",
+      lat: 45.49865436288935,
+      lng: -73.61837937589661,
+    },
+    {
+      pavillon: "Pavillon Roger-Gaudry",
+      lat: 45.502683040350085,
+      lng: -73.61583834278646,
+    },
+    {
+      pavillon: "Pavillon André-Aisenstadt",
+      lat: 45.501118321893316,
+      lng: -73.6158027337848,
+    },
+    {
+      pavillon: "Pavillon Claire-McNicoll",
+      lat: 45.50177979562059,
+      lng: -73.61654496899769,
+    },
+
+    {
+      pavillon: "Pavillon Lionel-Groulx",
+      lat: 45.4992286213827,
+      lng: -73.61811440328101,
+    },
+    {
+      pavillon: "Pavillon Marie-Victorin",
+      lat: 45.510619686954136,
+      lng: -73.61164866212512,
+    },
+    {
+      pavillon: "Pavillon Marguerite-D'Youville",
+      lat: 45.509398466364736,
+      lng: -73.61839316372183,
+    },
+    {
+      pavillon: "Pavillon Paul-G.-Desmarais",
+      lat: 45.500396996801676,
+      lng: -73.61633066502466,
+    },
+    {
+      pavillon: "Pavillon J.-A.-DeSève",
+      lat: 45.50714348820753,
+      lng: -73.61441639453919,
+    },
+    {
+      pavillon: "Pavillon de la Faculté de l'aménagement",
+      lat: 45.50463484932084,
+      lng: -73.62119293577463,
+    },
+    {
+      pavillon: "Pavillon de la Faculté de musique",
+      lat: 45.50929993324537,
+      lng: -73.60856773744851,
+    },
+    {
+      pavillon: "Pavillon J.-Armand-Bombardier",
+      lat: 45.50319960914957,
+      lng: -73.61299058051966,
+    },
+    {
+      pavillon: "Pavillon Marcelle-Coutu",
+      lat: 45.49999697231997,
+      lng: -73.61521747657558,
+    },
+    {
+      pavillon: "Pavillon Samuel-Bronfman",
+      lat: 45.49942661426102,
+      lng: -73.61633843067683,
+    },
+    {
+      pavillon: "Pavillon Maximilien-Caron",
+      lat: 45.49864611249791,
+      lng: -73.61698298364878,
+    },
+    {
+      pavillon: "Pavillon René-J.-A.-Lévesque",
+      lat: 45.5013693622242,
+      lng: -73.61503855240562,
+    },
+    {
+      pavillon: "Pavillon Liliane-de-Stewart",
+      lat: 45.50972366912569,
+      lng: -73.61924582738257,
+    },
+    {
+      pavillon: "Pavillon de la Direction des immeubles",
+      lat: 45.50216858742022,
+      lng: -73.61397216405996,
+    },
+    { pavillon: "Campus Mil", lat: 45.5231172783649, lng: -73.61966164943738 },
   ]);
 
   const [location, setLocation] = useState<Location.LocationObject | null>(
@@ -70,7 +142,31 @@ Pavillon de la Direction des immeubles
   );
   const [sortedPavillons, setSortedPavillons] = useState<string[]>([]);
 
+  const [appState, setAppState] = useState<AppStateStatus>(
+    AppState.currentState
+  );
+  const [isBackToForeground, setIsBackToForeground] = useState(true);
+
   useEffect(() => {
+    async function getCurrentLocation() {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+  
+        if (status !== "granted") {
+          console.info("Permission to access location was denied");
+          return;
+        }
+  
+        let position = await Location.getCurrentPositionAsync({});
+  
+        setLocation(position);
+        console.log("Current Location: ", position);
+      }
+      console.log("FIRST USE EFFECT");
+      getCurrentLocation();
+  }, []);
+
+  useEffect(() => {
+    console.log("SECOND USE EFFECT");
     async function getCurrentLocation() {
       let { status } = await Location.requestForegroundPermissionsAsync();
 
@@ -79,36 +175,57 @@ Pavillon de la Direction des immeubles
         return;
       }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-      console.info("User Location: ", location);
+      let position = await Location.getCurrentPositionAsync({});
+
+      setLocation(position);
+      console.log("Current Location: ", position);
     }
 
-   
-    getCurrentLocation();
-  }, []);
+    const handleAppStateChange = (nextAppState: AppStateStatus) => {
+      if (appState.match(/inactive|background/) && nextAppState === "active") {
+        console.info("App has come back to the foreground!");
+        setIsBackToForeground(true);
+        getCurrentLocation();
+        console.log("-----------------------------------------------------");
+        console.log("-----------------------------------------------------");
+        console.log("-----------------------------------------------------");
+      } else {
+        setIsBackToForeground(false);
+      }
+      setAppState(nextAppState);
+    };
+
+    const subscription = AppState.addEventListener(
+      "change",
+      handleAppStateChange
+    );
+
+    return () => {
+      subscription.remove();
+    };
+  }, [appState]);
 
   useEffect(() => {
     if (location && coordinates.length > 0) {
-        const userLat = location.coords.latitude;
-        const userLng = location.coords.longitude;
-    
-        const sorted = coordinates
-          .map((coord) => ({
-            ...coord,
-            distance: getDistanceFromLatLonInKm(
-              userLat,
-              userLng,
-              coord.lat,
-              coord.lng
-            ),
-          }))
-          .sort((a, b) => a.distance - b.distance)
-          .map((coord) => coord.pavillon);
-    
-        setSortedPavillons(sorted);
-        console.warn("Sorted Pavillons by Distance: ", sorted);
-      }    
+      const userLat = location.coords.latitude;
+      const userLng = location.coords.longitude;
+
+      const sorted = coordinates
+        .map((coord) => ({
+          ...coord,
+          distance: getDistanceFromLatLonInKm(
+            userLat,
+            userLng,
+            coord.lat,
+            coord.lng
+          ),
+        }))
+        .sort((a, b) => a.distance - b.distance)
+        .map((coord) => coord.pavillon);
+
+      setSortedPavillons(sorted);
+      console.warn("Sorted Pavillons by Distance: ", sorted);
+    }
   }, [location, coordinates]);
 
   // Function to calculate the distance between two coordinates
